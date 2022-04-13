c26a7005525d438799be12b7f5fca9feb14f6aed
sasikumari2i <sasikumar.raju@ideas2it.com>
2022-04-13 09:07:16 +0530

    13/4/22

--- inventorymanagementsystem/apps/orders/orderviews.py
+++ inventorymanagementsystem/apps/orders/orderviews.py
@@ -12,73 +12,30 @@ from ..products.serializers import ProductSerializer
 class OrderView(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    order_service = OrderService()
 
     def destroy(self, request, *args, **kwargs):
         super().destroy(request)
         return Response({"message" : "Order Deleted"})
 
-    @transaction.atomic()
     def create(self, request, *args, **kwargs):
-        products = Product.objects.all()
         order_products = request.data['orderproducts']
         request.data.pop('orderproducts')
         validated_data = OrderSerializer(data=request.data)
         validated_data.is_valid(raise_exception=True)
-        new_order = Order.objects.create(is_sales_order=validated_data.data['is_sales_order'],
-                                         #order_date=validated_data.data['order_date'],
-                                         delivery_date=validated_data.data['delivery_date'],
-                                         vendors_id=validated_data.data['vendors'],
-                                         customers_id=validated_data.data['customers'])
-
-        for product in order_products:
-            product_details = products.get(id=product['product'])
-            if validated_data.data['is_sales_order'] and product_details.available_stock >= product['quantity']:
-                product_details.available_stock = product_details.available_stock - product['quantity']
-            elif not validated_data.data['is_sales_order']:
-                product_details.available_stock = product_details.available_stock + product['quantity']
-            else:
-                raise Exception("Enter only available stock")
-            product_details.save()
-            order_product_data = OrderProduct.objects.create(order=new_order,product=product_details,
-                                                              quantity=product['quantity'])
-
-        order_product_data.save()
-        new_order.save()
+        new_order = self.order_service.create(validated_data, order_products)
         serialized = OrderSerializer(new_order)
         return Response(serialized.data)
 
-    @transaction.atomic
+
     def update(self, request, *args, **kwargs):
         order_details = self.get_object()
         order_products = request.data['orderproducts']
         request.data.pop('orderproducts')
         validated_data = OrderSerializer(data=request.data)
         validated_data.is_valid(raise_exception=True)
-
-        order_details.is_sales_order = validated_data.data['is_sales_order']
-        order_details.order_date = validated_data.data['order_date']
-        order_details.delivery_date = validated_data.data['delivery_date']
-        order_details.vendors_id = validated_data.data['vendors']
-        order_details.customers_id = validated_data.data['customers']
-        order_details.save()
-
-        for product in order_products:
-            product_details = Product.objects.get(id=product['product'])
-            if validated_data.data['is_sales_order'] and product_details.available_stock >= product['quantity']:
-                product_details.available_stock = product_details.available_stock - product['quantity']
-            elif not validated_data.data['is_sales_order']:
-                product_details.available_stock = product_details.available_stock + product['quantity']
-            else:
-                raise Exception("Enter only available stock")
-            product_details.save()
-            order_product_details = OrderProduct.objects.get(product=product_details.id,
-                                                             order=order_details.id)
-            order_product_details.product = product_details
-            order_product_details.order = order_details
-            order_product_details.quantity = product['quantity']
-            order_product_details.save()
-
-        serializer = OrderSerializer(order_details)
+        order = self.order_service.update(order_details, validated_data, order_products)
+        serializer = OrderSerializer(order)
         return Response(serializer.data)
 
 
@@ -89,46 +46,3 @@ class CustomerView(viewsets.ModelViewSet):
 class VendorView(viewsets.ModelViewSet):
     queryset = Vendor.objects.all()
     serializer_class = VendorSerializer
-
-
-# class OrderProductView(viewsets.ModelViewSet):
-#     queryset = OrderProduct.objects.all()
-#     serializer_class = OrderProductSerializer
-
-
-# @api_view(['POST'])
-# def add_orders(request):
-#     """To Add new sales or purchase order"""
-#
-#     validated_data = OrderSerializer(data=request.data)
-#     validated_data.is_valid(raise_exception=True)
-#     order_service = OrderService()
-#     order_details = order_service.add_orders(request.data)
-#
-#     return Response(order_details)
-#
-# @api_view(['GET'])
-# def get_order_by_id(request, order_id):
-#     """Get order details for the given order id"""
-#
-#     order_service = OrderService()
-#     order_data = order_service.get_order_id(order_id)
-#     return Response(order_data)
-#
-#
-# @api_view(['GET'])
-# def get_all(request):
-#     """Get details of all the orders placed"""
-#
-#     order_service = OrderService()
-#     order_data = order_service.get_all_orders()
-#     return Response(order_data)
-#
-#
-# @api_view(['PUT'])
-# def update_order(request, order_id):
-#     """Update details of the order with the given data"""
-#
-#     order_service = OrderService()
-#     order_data = order_service.update_order(order_id, request.data)
-#     return Response(order_data)
--- inventorymanagementsystem/apps/orders/service.py
+++ inventorymanagementsystem/apps/orders/service.py
@@ -13,58 +13,56 @@ class OrderService:
     """Performs order related operations like add new order, get single order,
     get all orders, update an order and delete order"""
 
-    products = Product.objects.all()
-
-    @transaction.atomic
-    def add_orders(self, validated_data):
-        """To Add new sales or purchase order from the given data"""
-
+    @transaction.atomic()
+    def create(self, validated_data, order_products):
         products = Product.objects.all()
-        order_products = validated_data['orderproducts']
-        validated_data.pop('orderproducts')
-        order = Order.objects.create(**validated_data)
+        new_order = Order.objects.create(is_sales_order=validated_data.data['is_sales_order'],
+                                         # order_date=validated_data.data['order_date'],
+                                         delivery_date=validated_data.data['delivery_date'],
+                                         vendors_id=validated_data.data['vendors'],
+                                         customers_id=validated_data.data['customers'])
 
         for product in order_products:
             product_details = products.get(id=product['product'])
-            if validated_data.get('is_sales_order') and product_details.available_stock >= product['quantity']:
+            if validated_data.data['is_sales_order'] and product_details.available_stock >= product['quantity']:
                 product_details.available_stock = product_details.available_stock - product['quantity']
-            elif not validated_data.get('is_sales_order'):
+            elif not validated_data.data['is_sales_order']:
                 product_details.available_stock = product_details.available_stock + product['quantity']
             else:
                 raise Exception("Enter only available stock")
             product_details.save()
-            order_product_data = OrderProduct.objects.create(order=order,product=product_details,
-                                                              quantity=product['quantity'])
-        order_response = OrderSerializer(order)
+            order_product_data = OrderProduct.objects.create(order=new_order, product=product_details,
+                                                             quantity=product['quantity'])
 
-        return order_response.data
+        order_product_data.save()
+        new_order.save()
 
-    def get_order_id(self,order_id):
-        """Get order details for the given order id"""
+        return new_order
 
-        order_data = Order.objects.get(id=order_id)
-        order_serializer = OrderSerializer(order_data)
-        return order_serializer.data
+    @transaction.atomic
+    def update(self, order_details, validated_data, order_products):
 
-    def get_all_orders(self):
-        """Get details of all the orders placed"""
+        order_details.is_sales_order = validated_data.data['is_sales_order']
+        order_details.order_date = validated_data.data['order_date']
+        order_details.delivery_date = validated_data.data['delivery_date']
+        order_details.vendors_id = validated_data.data['vendors']
+        order_details.customers_id = validated_data.data['customers']
+        order_details.save()
 
-        orders = Order.objects.all()
-        order_serializer = OrderSerializer(orders, many=True)
-        return order_serializer.data
+        for product in order_products:
+            product_details = Product.objects.get(id=product['product'])
+            if validated_data.data['is_sales_order'] and product_details.available_stock >= product['quantity']:
+                product_details.available_stock = product_details.available_stock - product['quantity']
+            elif not validated_data.data['is_sales_order']:
+                product_details.available_stock = product_details.available_stock + product['quantity']
+            else:
+                raise Exception("Enter only available stock")
+            product_details.save()
+            order_product_details = OrderProduct.objects.get(product=product_details.id,
+                                                             order=order_details.id)
+            order_product_details.product = product_details
+            order_product_details.order = order_details
+            order_product_details.quantity = product['quantity']
+            order_product_details.save()
 
-    def update_order(self,order_id, order_data):
-        """Update details of the order with the given data"""
-
-        order_details = OrderSerializer(data=order_data)
-        # if order_details.is_valid():
-        #     order_details.save()
-        #     print("Is Valid")
-
-        # order_details = Order.objects.get(id=order_id)
-        # for key, value in order_data.items():
-        #     if order_details.__getattribute__(key):
-        #         order_details.__setattr__(key, value)
-        #     order_details.save()
-        order_serializer = OrderSerializer(order_details)
-        return order_serializer.data
+            return order_details
