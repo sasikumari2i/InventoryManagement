7f79bdada549a5e617bb525448f7577174b88172
sasikumari2i <sasikumar.raju@ideas2it.com>
2022-04-29 22:17:52 +0530

    first commit

--- inventorymanagementsystem/apps/payments/views.py
+++ inventorymanagementsystem/apps/payments/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from rest_framework import viewsets, generics
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
